678a8002d94cdcd33f5802368b7a089b8db8f626
ryang <ryang@etudiant.univ-rennes1.fr>
2023-03-31 22:24:33 +0200

    tp end without bonus

--- src/controller/ControleurTP6.java
+++ src/controller/ControleurTP6.java
@@ -4,12 +4,15 @@ import java.sql.SQLException;
 import java.util.Random;
 import java.util.stream.IntStream;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 import javafx.scene.control.Tooltip;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import models.MotsCroisesTP6;
 
@@ -25,57 +28,80 @@ public class ControleurTP6 {
 	private GridPane grilleMC;
 
 	@FXML
+	private Button nouvelleGrille;
+
+	@FXML
 	private void initialize() throws SQLException {
 		cg = new ChargerGrille();
-
+		nouvelleGrille = new Button("nouvelle grille");
 		// modele = MotsCroisesFactory.creerMotsCroises2x3();
 		initTF();
-
+		grilleMC.add(nouvelleGrille, grilleMC.getRowCount(), 0);
 		for (Node n : grilleMC.getChildren()) {
 			if (n instanceof TextField) {
 				TextField tf = (TextField) n;
-				int lig = ((int) n.getProperties().get("gridpane-row")) + 1;
-				int col = ((int) n.getProperties().get("gridpane-column")) + 1;
-				// Initialisation du TextField tf ayant pour coordonnées (lig, col)
-				// (cf. sections 1.3, 1.4 et 1.5)
-				tf.textProperty().bindBidirectional(modele.propositionProperty(lig, col));
-				String defH = modele.getDefinition(lig, col, true);
-				String defV = modele.getDefinition(lig, col, false);
-				String texte = "";
-				if (defH == null) {
-					if (defV == null) {
-						texte = "il n'y a pas de definition";
-					} else {
-						texte = defV;
+				/*
+				 * tf.setOnMouseClicked((e) -> { this.clicLettre(e); });
+				 */
+				tf.setOnKeyPressed((e) -> {
+					this.keyLettre(e);
+				});
+			}
+			if (n instanceof Button) {
+				Button bt = (Button) n;
+				bt.setOnAction(actionEvent -> {
+					try {
+						initialize();
+					} catch (SQLException e) {
+						e.printStackTrace();
 					}
-				} else {
-					if (defV == null) {
-						texte = defH;
-					} else {
-						texte = defH + " / " + defV;
-					}
-				}
-				tf.setTooltip(new Tooltip(texte));
-				tf.setOnMouseClicked((e) -> {
-					this.clicLettre(e);
 				});
 			}
 		}
-
 	}
 
 	@FXML
-	public void clicLettre(MouseEvent e) {
-		if (e.getButton() == MouseButton.MIDDLE) {
-			// C'est un clic "central"
-			TextField tf = (TextField) e.getSource();
-			int lig = ((Integer) tf.getProperties().get("gridpane-row")) + 1;// n° ligne de la case (cf. boucle du 1.2)
-			int col = ((Integer) tf.getProperties().get("gridpane-column")) + 1;// n° colonne de la case (cf. boucle du
-																				// 1.2)
-			// appel de montrerSolution() sur le modèle
-			modele.montrerSolution(lig, col);
+	public void keyLettre(KeyEvent e) {
+		TextField nextTf = new TextField();
+		TextField tf = (TextField) e.getSource();
+		switch (e.getCode()) {
+		case UP:
+			break;
+		case DOWN:
+			break;
+		case LEFT:
+			break;
+		case RIGHT:
+			break;
+		default:
+			int lig = ((int) tf.getProperties().get("gridpane-row")) + 1;
+			int col = ((int) tf.getProperties().get("gridpane-column")) + 1;
+			ObservableList<Node> tfs = grilleMC.getChildren();
+			int i = 0;
+
+			nextTf = (TextField) tfs.get(tfs.size() - 2);
+			while (!((((int) nextTf.getProperties().get("gridpane-row") + 1) == lig)
+					&& (((int) nextTf.getProperties().get("gridpane-column") + 1) == col))) {
+				nextTf = (TextField) tfs.get(i);
+				i++;
+			}
+			nextTf = (TextField) tfs.get(i);
 		}
+		tf.focusedProperty().addListener((arg0, oldValue, newValue) -> {
+			tf.setText(e.getCode().toString());
+		});
+		tf.setStyle("-fx-focus-color: blue; -fx-faint-focus-color: blue; -fx-text-fill: black;");
+		nextTf.requestFocus();
+		;
 	}
+
+	/*
+	 * @FXML public void clicLettre(MouseEvent e) { if (e.getButton() ==
+	 * MouseButton.MIDDLE) { // C'est un clic "central" TextField tf = (TextField)
+	 * e.getSource(); int lig = // n° ligne de la case (cf. boucle du 1.2) int col =
+	 * // n° colonne de la case (cf. boucle du 1.2) // appel de montrerSolution()
+	 * sur le modèle // modele.montrerSolution(lig, col); } }
+	 */
 
 	private void initTF() throws SQLException {
 		modeleTF = (TextField) grilleMC.getChildren().get(0);
@@ -89,14 +115,53 @@ public class ControleurTP6 {
 			for (int col = 0; col < modele.getLargeur(); col++) {
 				if (!(modele.estCaseNoire(lig + 1, col + 1))) {
 					TextField tf = new TextField();
-					tf.setPrefWidth(modeleTF.getPrefWidth());
-					tf.setPrefHeight(modeleTF.getPrefHeight());
-					for (Object cle : modeleTF.getProperties().keySet()) {
-						tf.getProperties().put(cle, modeleTF.getProperties().get(cle));
+					tf = setTF();
+					tf.textProperty().bindBidirectional(modele.propositionProperty(lig + 1, col + 1));
+					String defH = modele.getDefinition(lig + 1, col + 1, true);
+					String defV = modele.getDefinition(lig + 1, col + 1, false);
+					String texte = "";
+					if (defH == null) {
+						if (defV == null) {
+							texte = "il n'y a pas de definition";
+						} else {
+							texte = defV;
+						}
+					} else {
+						if (defV == null) {
+							texte = defH;
+						} else {
+							texte = defH + " / " + defV;
+						}
 					}
+					tf.setTooltip(new Tooltip(texte));
+					tf.clear();
 					grilleMC.add(tf, col, lig);
 				}
 			}
 		}
+	}
+
+	private TextField setTF() {
+		TextField tf = new TextField();
+		tf.setPrefWidth(modeleTF.getPrefWidth());
+		tf.setPrefHeight(modeleTF.getPrefHeight());
+		for (Object cle : modeleTF.getProperties().keySet()) {
+			tf.getProperties().put(cle, modeleTF.getProperties().get(cle));
+		}
+		tf.setTextFormatter(new TextFormatter<String>((Change change) -> {
+			String newText = change.getControlNewText();
+			if (newText.length() > 1) {
+				return null;
+			} else {
+				return change;
+			}
+		}));
+		tf.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.matches("\\sa-zA-Z*")) {
+				tf.setText(newValue.replaceAll("[^\\sa-zA-Z]", ""));
+			}
+		});
+		tf.setStyle("-fx-focus-color: blue; -fx-faint-focus-color: blue; -fx-text-fill: white;");
+		return tf;
 	}
 }
